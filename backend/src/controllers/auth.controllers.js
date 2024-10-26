e3dd84567ead1c372edc249dc914af56e2559f81
Da Silva Felix Bruno <brunolok79@gmai.com>
2024-10-25 21:59:19 -0300

    se elimino el backend para centrame en la practica de react

--- backend/src/controllers/auth.controllers.js
+++
@@ -1,7 +0,0 @@
-// import generateJWT from "../helpers/generateJWT.js";
-// import connections from "../db/database";
-
-export const register = (req, res) => {
-  const { username, password, email } = req.body;
-  console.log(username, password, email);
-};
--- backend/src/middlewares/validatorJWT.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const validatorJWT = (req, res, next) => {
-  console.log(req.cookies);
-  const token = req.cookies.authtoken;
-  console.log(token);
-  next();
-};
-export default validatorJWT;
